e8cb6bf4e45940c2a0a9892dbd6d1160345316f8
App Academy Student <student@appacademy.io>
2016-10-14 13:53:31 -0700

    Finish inherit exercise

--- inheritanceExercises/inherits.js
+++ inheritanceExercises/inherits.js
@@ -15,4 +15,23 @@ function Asteroid () {};
 Asteroid.inherits(MovingObject);
 
 
-MovingObject.prototype.move = function(){ console.log("I've moved!")}
+MovingObject.prototype.move = function() { console.log("I've moved!") };
+Ship.prototype.fire = function() { console.log("Pew! Pew!") };
+Asteroid.prototype.smash = function() { console.log("BRRRRRMMMMMM") };
+
+
+const ship = new Ship;
+const asteroid = new Asteroid;
+const movingObject = new MovingObject;
+
+ship.move()
+ship.fire()
+// ship.smash()
+
+asteroid.move()
+// asteroid.fire()
+asteroid.smash()
+
+movingObject.move()
+// movingObject.fire()
+// movingObject.smash()
